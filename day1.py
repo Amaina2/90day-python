99a46a6863a4cac8fd838494561bcf5fb4bef120
amaina2 <mainaaustin05@gmail.com>
2026-07-21 14:01:33 +0300

    initial commit

---
+++ day1.py
@@ -0,0 +1,8 @@
+print ("Hello world")
+print ("My name is Austin Maina!")
+print (16)
+
+age = 21
+print ("My name is Austin Maina and I am", age, "years old.")
+print (f"My name is Austin Maina and I am {age} years old.")
+print ("My name is Austin Maina and I am " + str(age) + " years old.")
